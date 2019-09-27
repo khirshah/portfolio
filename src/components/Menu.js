8b608ffebed898fadc2a0564e165ee4826785dbc
khirshah <khirshah@gmail.com>
2019-09-27 18:26:42 +0100

    links added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,6 +8,8 @@ export default class Menu extends Component {
                 <li><a href="https://agi-react-calculator.herokuapp.com/">Calculator</a></li>
                 <li><a href="http://khirshah.fractalometry.com/host-parasite-model/">Cellular automaton 1</a></li>
                 <li><a href="http://khirshah.fractalometry.com/competition-model/">Cellular automaton 2</a></li>
+                <li><a href="https://agi-react-calculator.herokuapp.com/">Weather app</a></li>
+                <li><a href="https://agi-react-calculator.herokuapp.com/">Ebay search</a></li>
                 <li><a href="/contact/">Contact</a></li>
             </ul>
         );
