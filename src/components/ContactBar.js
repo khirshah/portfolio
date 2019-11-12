d1061e439e2e84e63b4efd0638dd8b065bffe7c0
khirshah <khirshah@gmail.com>
2019-11-12 15:33:34 +0000

    hompage text adjusted, contactbar icon sequence modified, icon images replaced

--- src/components/ContactBar.js
+++ src/components/ContactBar.js
@@ -6,10 +6,10 @@ export default class ContactBar extends Component {
   render () {
     return (
       <div className={styles.contactBarContainer}>
-        <div className={styles.contactBar}> 
-          <ContactIcon name={"github"} link={'https://github.com/khirshah'}/>
+        <div className={styles.contactBar}>
+          <ContactIcon name={"gmail"} link={'mailto:agnes.meri.work@gmail.com'}/> 
           <ContactIcon name={"linkedin"} link={'https://www.linkedin.com/in/agnes-meri-a50ba4173/'}/>
-          <ContactIcon name={"gmail"} link={'mailto:agnes.meri.work@gmail.com'}/>
+          <ContactIcon name={"github"} link={'https://github.com/khirshah'}/>
           <ContactIcon name={"codingame"} link={'https://www.codingame.com/profile/58a1a525989e591466fe576355e423c30494952'}/>
           <ContactIcon name={"hackerrank"} link={'https://www.hackerrank.com/miyoko'}/>
         </div>
