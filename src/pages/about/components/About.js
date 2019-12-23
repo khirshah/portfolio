100372aeaa995408999fa7c4b21f307ab355197b
khirshah <khirshah@gmail.com>
2019-12-23 15:20:59 +0000

    feat: about page is mobile responsive

--- src/pages/about/components/About.js
+++ src/pages/about/components/About.js
@@ -14,9 +14,9 @@ export default class About extends Component {
             <h3 className={styles.title}>SKILLS</h3>
             <Skills/>
           </div>
-          <div className={styles.section}>
-            <h3 className={styles.title}>ARTICLES</h3>
-            <div className={styles.contContainer}>
+          <div className={`${styles.section} w-100`}>
+            <h3 className={styles.title}>PUBLICATIONS</h3>
+            <div className={styles.publContainer}>
               <Pdf title="MODELLING THE SPATIAL AND TEMPORAL DISPERSAL OF Cuscuta europea (Cuscutaceae)" 
                    link="/assets/docs/MA_KJ_paper_PolishJE_coloured.pdf"
                    image="/assets/images/paperxhdpi.png"/>
