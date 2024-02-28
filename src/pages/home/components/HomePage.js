271b529501b58198141c2a2a0ff50cd087bc9a99
khirshah <khirshah@gmail.com>
2024-02-28 20:17:12 +0000

    change home page intro text, fix links

--- src/pages/home/components/HomePage.js
+++ src/pages/home/components/HomePage.js
@@ -8,15 +8,19 @@ import About from "../../about/components/About";
 
 export default class HomePage extends Component {
   render () {
-    const gitLogo = {backgroundImage: "url(/assets/icons/contact/GitHub/GitHub-Mark-Light-120px-plus.png)"};
-    console.log('homepage')
     return (
-      <Page styles={styles.homepage}> 
+      <Page> 
           <div className={styles.textContainer}>
             <p>Welcome to my website!</p>
-            <p>Feel free to have a look at my <a href="/projects/">projects</a>,</p>
-            <p>or check out my <a href="/about/">skills and achievments.</a></p>
-            <p>You are welcome to browse my projects on</p>
+            <p>I'm a mid level frontend engineer with some years of backend experience</p>
+            <p>
+              <span>
+              The areas I'm passionate about are testing, data handling (currently Redux Toolkit), React component logic, TypeScript, routing and authentication.
+              </span>
+            </p>
+            <p>Have a look at my <a href="#projects">pet projects</a>,</p>
+            <p>and check out my <a href="#about">skills and achievments.</a></p>
+            <p>You can also browse my projects on</p>
             <p>my github page:</p>
           </div>
           <GitHub gitlink="https://github.com/khirshah/"/>
