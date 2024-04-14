97179e0a0340604a2ed6e5705ad3f515f5439277
agi <khirshah@gmail.com>
2024-04-14 23:02:39 +0100

    Make video styles mobile responsive

--- src/pages/about/components/Video.js
+++ src/pages/about/components/Video.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import styles from "../styles/pdf.css";
+import videoStyles from "../styles/about.css"
 
 export default class Video extends Component {
   render () {
     return (
       <div className={styles.pdfContainer}>
-        <a name="video"></a>
-        <iframe 
-          className={styles.video} 
-          src="https://drive.google.com/file/d/1RNcP5anNGDfxi_Mr4a441DX6Z3gR1XhD/preview"
-          width="400" 
-          height="240"
-        /> 
+            <iframe 
+            className={videoStyles.video}
+            src="https://drive.google.com/file/d/1RNcP5anNGDfxi_Mr4a441DX6Z3gR1XhD/preview"
+            /> 
         <div className={styles.pdfTitle}>
           <div className={styles.text}>
               <p>
