a68121fea84fdeee1ae458c3e1d256b4877d7bee
Agi <agi.meri"equineregister.co.uk>
2025-10-25 16:05:25 +0100

    small changes

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Menu from './Menu'
 import ContactBar from './ContactBar'
-import styles from './page.css'
-export default class Page extends Component {
-  render() {
-    return (
-      <div className={`${styles.page} ${this.props.styles} container-fluid`}>
-        <div className={`${styles.pageHeader} d-flex col-12 p-0`}>
-          <p>
-            ÁGNES MÉRI
-            <br />
-            WEB DEVELOPER
-          </p>
-          <Menu />
-        </div>
-        <div className={styles.contentsContainer}>{this.props.children}</div>
-        <a name='contactBar'></a>
+import styles from './page.module.css'
+
+export default function Page({ styles: additionalStyles, children }) {
+  return (
+    <div className={`${styles.page} ${additionalStyles} container-fluid`}>
+      <div className={`${styles.pageHeader} d-flex col-12 p-0`}>
+        <p>
+          ÁGNES MÉRI
+          <br />
+          WEB DEVELOPER
+        </p>
+        <Menu />
+      </div>
+      <div className={styles.contentsContainer}>{children}</div>
+      <div id='contactBar'>
         <ContactBar />
       </div>
-    )
-  }
+    </div>
+  )
 }
