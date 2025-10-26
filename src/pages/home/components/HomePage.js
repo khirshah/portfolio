d14489ccb56857b3906d7f08ebcd5b340d93e07a
Agnes Meri <33101589+khirshah@users.noreply.github.com>
2025-10-26 12:55:11 +0000

    add intro text (#24)
    * add intro text
    
    * tidy up
    
    ---------
    
    Co-authored-by: Agi <agi.meri"equineregister.co.uk>

--- src/pages/home/components/HomePage.js
+++ src/pages/home/components/HomePage.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Page from '../../../components/Page'
-import GitHub from '../../../components/GitHub'
 import styles from '../styles/homepage.module.css'
 import Projects from '../../projects/components/Projects'
 import About from '../../about/components/About'
+import introText from '../../projects/data/introText'
 
 const HomePage = () => {
   return (
     <Page>
       <div className={styles.textContainer}>
-        <a href='#projects'></a>,<a href='#about'></a>
-        <a href='#other'></a>
+        <h2>{introText.greeting}</h2>
+        <p>{introText.body}</p>
+        <p>{introText.closing}</p>
       </div>
-      <GitHub gitlink='https://github.com/khirshah/' />
       <Projects />
       <About />
     </Page>
